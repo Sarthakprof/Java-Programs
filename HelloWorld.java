a932b89fa60c6e10d074d1e6fb957d5be4f03148
Sarthak Srivastava <sarthak.prof@gmail.com>
2019-12-14 23:09:49 +0530

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class HelloWorld{
+    public static void main(String arg[]) {
+        System.out.println("Hello World! My first java program!!");
+        Date date = new Date();
+        System.out.println("Todays date is " + date);
+    }
+}
