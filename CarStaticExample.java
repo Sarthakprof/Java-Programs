28075efe7db067bd6b5722ed2c8a368eb39b881f
Sarthak Srivastava <sarthak.prof@gmail.com>
2020-02-08 18:06:50 +0530

    All about Static - variables, methods, initialization blocks

---
+++ CarStaticExample.java
@@ -0,0 +1,48 @@
+
+
+public class CarStaticExample {
+
+	 public String color;
+	 public String type;
+	 //class variables
+	 public static int carCount =1;
+	 public static int serialNumber;
+	 
+	 
+	 CarStaticExample(){		 
+		 carCount++;
+		 serialNumber = carCount;
+	 }
+	    CarStaticExample(String c, String t){
+	        color = c;
+	        type = t;
+	        carCount++;
+			serialNumber = carCount;
+	    }
+
+	    public void start(){
+	        System.out.println("Get Started");
+	    }
+
+	    public String printDesc(){
+	       String desc = "The color of my Car is " + color + " and the type of my Car is " + type;
+	        return desc;
+	    }
+
+	    public static void main(String arg[]) {
+	    	CarStaticExample car = new CarStaticExample("Blue", "Lamberghine Gallerado");
+	        car.start();
+	        String carDesc = car.printDesc();
+	        System.out.println(carDesc);   
+	        System.out.println("Serial Number of you Car is: " + serialNumber);
+	        
+	        //reset count value by call static method
+	        CarStaticExample.resetCount();
+	     }
+	    
+	    public static void resetCount() {
+	    	carCount=0;
+	    }
+	    
+	    
+}
