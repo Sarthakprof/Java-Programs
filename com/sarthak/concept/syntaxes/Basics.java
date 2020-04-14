bd8241a5214a7d0423657d7cd8aeea1ea3a29270
Sarthak Srivastava <sarthak.prof@gmail.com>
2020-04-14 23:21:16 +0530

    added increment, ternary, and precedence operators

--- com/sarthak/concept/syntaxes/Basics.java
+++ com/sarthak/concept/syntaxes/Basics.java
@@ -37,6 +37,38 @@ public class Basics {
 			str = "unknown";
 		}
 		System.out.println(str);
+		
+		//increment operators
+		int x = 0;
+		System.out.println(x++); // 0
+		System.out.println(x); // 1
+		System.out.println(++x); // 2
+		System.out.println(x); // 2
+		 
+		int y = 0;
+		y++;
+		System.out.println(y); // 1 - returns new value
+		
+		int z = 0;
+		z = z++;
+		System.out.println(z); // 0 - returns old value
+		
+		//ternary operator
+		int x1 = 2;
+		int y1 = 3;
+		
+		String s = x1>y1 ? "x is greater" : "y is greater";
+		System.out.println(s);
+		
+		// precedence example
+		
+		int r = 2 + 3 * 5;
+		int d = (2+3) * 5;
+		
+		System.out.println(r); // 17
+		System.out.println(d); // 25
 	}
+
+	
 
 }
