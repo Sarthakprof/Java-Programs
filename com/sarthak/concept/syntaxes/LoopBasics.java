153470b98ea6d713d874063563db8549d99d5c09
Sarthak Srivastava <sarthak.prof@gmail.com>
2020-04-15 00:50:20 +0530

    added break, continue and label examples

--- com/sarthak/concept/syntaxes/LoopBasics.java
+++ com/sarthak/concept/syntaxes/LoopBasics.java
@@ -36,6 +36,47 @@ public class LoopBasics {
 				System.out.println(z);
 			}
 		}
+		
+		// "break" stmt
+		// break stmt tells whatever loop i am in, i want break that loop
+		// example
+		System.out.println("Break - Example");
+		int a = 0;
+		while(true) {
+			System.out.println(a++);
+			if(a == 10) {
+				break;
+			}
+		}
+		
+		// "continue example"
+		System.out.println("Continue - Example");
+		for(int k=0; k<=22; k++) {
+			if(k%7==0) {
+				continue;
+			}
+			System.out.println(k+" is not divisible by 7");
+		}
+		
+		// "Label" Example
+		System.out.println("Label - Example");
+		//below code will cause infinite loop of outer loop even if inner while loop is set break;
+		/*
+		 * while(true) { int r=0; while(true) { if(r>=10) { break; }else {
+		 * System.out.println(r++); } } }
+		 */
+		// the above code can be fixed using "label"
+		outer_label:
+		while (true) {
+			int r = 0;
+			while (true) {
+				if (r >= 10) {
+					break outer_label;
+				} else {
+					System.out.println(r++);
+				}
+			}
+		}
 
 	}
 }
