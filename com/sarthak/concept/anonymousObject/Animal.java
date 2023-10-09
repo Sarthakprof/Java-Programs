5d49eef6718eece8361806f43e55be7d45af2933
Sarthak Srivastava <38829053+Sarthakprof@users.noreply.github.com>
2023-10-09 18:52:32 +0100

    anonymous object

---
+++ com/sarthak/concept/anonymousObject/Animal.java
@@ -0,0 +1,8 @@
+package com.sarthak.concept.anonymousObject;
+
+public class Animal {
+
+    public void showAnimal(){
+        System.out.println("I am Lion");
+    }
+}
---
+++ com/sarthak/concept/anonymousObject/AnimalDemo.java
@@ -0,0 +1,9 @@
+package com.sarthak.concept.anonymousObject;
+
+public class AnimalDemo {
+    public static void main(String[] args) {
+
+        //Anonymous Object
+        new Animal().showAnimal();
+    }
+}
